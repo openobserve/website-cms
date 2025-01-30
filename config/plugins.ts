0f4eb3b493f4136cad940395e8a1d43c3840fb29
Kirtan Thakkar <kirtan@kiara.tech>
2025-01-30 14:03:05 +0530

    fix: plugins ts file

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,7 +1,26 @@
-export default () => ({
+export default ({ env }) => ({
   "strapi-v5-plugin-populate-deep": {
     config: {
       defaultDepth: 10, // Default is 5
+    },
+  },
+  upload: {
+    config: {
+      provider: "aws-s3",
+      providerOptions: {
+        rootPath: "assets",
+        s3Options: {
+          region: env("AWS_REGION"),
+          params: {
+            Bucket: env("AWS_BUCKET"),
+          },
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
     },
   },
 });
