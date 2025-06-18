f8672a074d90d9f0b433f589aab0683fd4446a87
krupa <krupa.jivani@kiara.tech>
2025-06-18 11:45:01 +0530

    feat: Make a collection type for the webinar, tags,authors

---
+++ src/api/webinar-tag/controllers/webinar-tag.ts
@@ -0,0 +1,7 @@
+/**
+ * webinar-tag controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::webinar-tag.webinar-tag');
---
+++ src/api/webinar-tag/services/webinar-tag.ts
@@ -0,0 +1,7 @@
+/**
+ * webinar-tag service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::webinar-tag.webinar-tag');
