0a8bf1f7702d14ff6fe785c95f6b020da1d0698a
krupa <krupa.jivani@kiara.tech>
2025-06-18 17:53:26 +0530

    fix: update video type to string

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -1448,7 +1448,7 @@ export interface ApiWebinarPostWebinarPost extends Struct.CollectionTypeSchema {
     updatedAt: Schema.Attribute.DateTime;
     updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
-    video: Schema.Attribute.Media<'files' | 'videos'>;
+    video: Schema.Attribute.String;
   };
 }
 
