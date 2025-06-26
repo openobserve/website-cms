ca47bfcc456b45bd691b979459a76176cb5fa843
krupa <krupa.jivani@kiara.tech>
2025-06-26 15:04:49 +0530

    feat: add component to the download page v2 single type

---
+++ src/api/download-page-v2/routes/download-page-v2.ts
@@ -0,0 +1,7 @@
+/**
+ * download-page-v2 router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::download-page-v2.download-page-v2');
---
+++ src/api/download-page-v2/services/download-page-v2.ts
@@ -0,0 +1,7 @@
+/**
+ * download-page-v2 service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::download-page-v2.download-page-v2');
