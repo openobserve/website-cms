09968b3a038f8fff9dd82af7f814631ebb98a1c1
krupa <krupa.jivani@kiara.tech>
2025-09-29 08:00:05 -0400

    remove unused fields from the strapi for blog and webinar

--- src/api/webinar-tag/routes/webinar-tag.ts
+++
@@ -1,7 +0,0 @@
-/**
- * webinar-tag router
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::webinar-tag.webinar-tag');
--- src/api/webinar-tag/services/webinar-tag.ts
+++
@@ -1,7 +0,0 @@
-/**
- * webinar-tag service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::webinar-tag.webinar-tag');
