1b0abcc5a0adba7518142d07c367119230c6d891
krupa <krupa.jivani@kiara.tech>
2025-12-19 10:45:56 +0530

    feat: remove experiment collection and related files

--- src/api/experiment/controllers/experiment.ts
+++
@@ -1,7 +0,0 @@
-/**
- * experiment controller
- */
-
-import { factories } from "@strapi/strapi";
-
-export default factories.createCoreController("api::experiment.experiment");
--- src/api/experiment/services/experiment.ts
+++
@@ -1,7 +0,0 @@
-/**
- * experiment service
- */
-
-import { factories } from "@strapi/strapi";
-
-export default factories.createCoreService("api::experiment.experiment");
